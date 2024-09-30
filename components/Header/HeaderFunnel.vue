f3b770984bc6be65c421f37ba0c7b165a574328c
Osman Turan <turan@anwalt.de>
2024-09-30 10:24:56 +0200

    add app shell

--- components/Header/HeaderFunnel.vue
+++ components/Header/HeaderFunnel.vue
@@ -15,6 +15,10 @@ const isAuthenticatedCopy = ref(props.isAuthenticated)
         <svg class="inline-block w-auto h-4 text-neutral-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256">
           <path d="M208,80H176V56a48,48,0,0,0-96,0V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80Zm-80,84a12,12,0,1,1,12-12A12,12,0,0,1,128,164Zm32-84H96V56a32,32,0,0,1,64,0Z"></path>
         </svg>
+        <p>lsöadkölaskdlöaskdölsk</p>
+        <p>lsöadkölaskdlöaskdölsk</p>
+        <p>lsöadkölaskdlöaskdölsk</p>
+        <p>lsöadkölaskdlöaskdölsk</p>
         <span v-if="isAuthenticatedCopy" class="text-neutral-500 ml-2 hidden sm:inline anw-text-p1">logged in</span>
         <span v-if="!isAuthenticatedCopy" class="text-neutral-500 ml-2 hidden sm:inline anw-text-p1">logged out</span>
       </div>
