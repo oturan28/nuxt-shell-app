ca893f73b580ebbbd914a955e5c43b057fbfaff2
Osman Turan <turan@anwalt.de>
2024-11-29 07:20:43 +0100

    add app shell

--- components/Header/Default.vue
+++ components/Header/Default.vue
@@ -28,7 +28,7 @@ const showDropdown = ref(false)
                 <img class="size-8 rounded-full" src="https://www.anwalt.de/cdn-cgi/image/fit=cover,width=80,height=80/img/profile/placeholder-avatar-male-180.png" alt="">
               </button>
             </div>
-            <div v-if="showDropdown" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+            <div v-show="showDropdown" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
               <slot name="loginStateDropdown"></slot>
               <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
               <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
