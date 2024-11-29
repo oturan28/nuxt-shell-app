c7fa9ea5c3057f48035b40fbd964c60929e88358
Osman Turan <turan@anwalt.de>
2024-11-29 06:35:06 +0100

    add app shell

---
+++ components/Layout/Default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="min-h-screen flex flex-col">
+    <slot id="header">
+      <HeaderDefault />
+    </slot>
+    <slot id="content" class="flex grow">
+      <div class="w-full">
+
+      </div>
+    </slot>
+  </div>
+  <slot id="footer">
+    <FooterDefault />
+  </slot>
+</template>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="min-h-screen flex flex-col">
-    <HeaderDefault />
-    <slot id="content" class="flex grow">
-      <div class="w-full">
-
-      </div>
-    </slot>
-  </div>
-  <FooterDefault />
+<LayoutDefault />
 </template>
 
