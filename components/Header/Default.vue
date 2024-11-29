bc0fc2b36fc6b008af99760e5bcc80dac9001afe
Osman Turan <turan@anwalt.de>
2024-11-29 07:14:42 +0100

    add app shell

--- components/Header/Default.vue
+++ components/Header/Default.vue
@@ -25,7 +25,7 @@ const showDropdown = ref(false)
               <button @click="showDropdown = !showDropdown" type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                 <span class="absolute -inset-1.5"></span>
                 <span class="sr-only">Open user menu</span>
-                <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <img class="size-8 rounded-full" src="https://www.anwalt.de/cdn-cgi/image/fit=cover,width=80,height=80/img/profile/placeholder-avatar-male-180.png" alt="">
               </button>
             </div>
             <div v-if="showDropdown" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
