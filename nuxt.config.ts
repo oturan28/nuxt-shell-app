68c410668418256d5f7b21da8d62ba9eee2a28f5
Osman Turan <turan@anwalt.de>
2024-12-02 14:54:02 +0100

    add oauthlogin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
   experimental: {
     componentIslands: 'local+remote'
   },
-
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-auth-utils']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-auth-utils'],
+  runtimeConfig: {
+    sessionPassword: process.env.NUXT_SESSION_PASSWORD,
+    oauthGithubClientId: process.env.OAUTH_GITHUB_CLIENT_ID,
+    oauthGithubClientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
+  }
 })
