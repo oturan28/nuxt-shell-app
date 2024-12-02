a06f78d792f27b5c15d8c2f829e58d9c16402605
Osman Turan <turan@anwalt.de>
2024-12-02 15:54:56 +0100

    add oauthlogin

--- pages/login.vue
+++ pages/login.vue
@@ -4,10 +4,9 @@ const { user } = useUserSession()
 </script>
 
 <template>
-  <div class="justify-center">
+  <div class="container mt-10 mx-auto">
     <a href="/api/auth/github" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       Login with github
     </a>
   </div>
-
 </template>
