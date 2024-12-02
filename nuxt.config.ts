59bc4bf0560cdecc61f68883d9fb29f7f0036737
Osman Turan <turan@anwalt.de>
2024-12-02 20:08:32 +0100

    add oauthlogin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-auth-utils'],
-  runtimeConfig: {
-    oauth: {
-      github: {
-        clientId: 'Ov23liWEq2ZhhpT0yLvU',
-        clientSecret: '6678d4f641a945cb131288e7402fcc43d6ceb707',
+  $production:{
+    runtimeConfig: {
+      oauth: {
+        github: {
+          clientId: 'Ov23liNmZ7G59mRd5aq7',
+          clientSecret: 'f6bd638880e4db1074a74b447d7b9b5ed38235fe',
+        }
       }
     }
-  }
+  },
+  $development: {
+    runtimeConfig: {
+      oauth: {
+        github: {
+          clientId: 'Ov23liWEq2ZhhpT0yLvU',
+          clientSecret: '6678d4f641a945cb131288e7402fcc43d6ceb707',
+        }
+      }
+    }
+  },
 })
